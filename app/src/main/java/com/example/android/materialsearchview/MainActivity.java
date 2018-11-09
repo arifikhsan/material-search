1e6a4cd1fa18d7100d7178f2234839f02b738280
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2018-11-09 07:46:24 +0700

    initial commit

---
+++ app/src/main/java/com/example/android/materialsearchview/MainActivity.java
@@ -0,0 +1,94 @@
+package com.example.android.materialsearchview;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    List<String> list;
+    MaterialSearchView searchView;
+    ListView listView;
+    ArrayAdapter adapter;
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Material SearchView");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onClick(View view) {
+                finishAfterTransition();
+            }
+        });
+
+        listView = findViewById(R.id.listView);
+        searchView = findViewById(R.id.searchView);
+
+        prepareData();
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_expandable_list_item_1, list);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Snackbar.make(view, "Single Clicked on: " + adapter.getItem(position).toString(), Snackbar.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_search, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.search);
+
+        searchView.setMenuItem(searchItem);
+        searchView.setHint("Search...");
+        searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                searchView.clearFocus();
+                searchView.closeSearch();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+//                List<String> newList = new ArrayList<>();
+                adapter.getFilter().filter(newText);
+                return true;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void prepareData() {
+        list = new ArrayList<>();
+        list.add("Alpha");
+        list.add("Beta");
+        list.add("CupCake");
+    }
+}
